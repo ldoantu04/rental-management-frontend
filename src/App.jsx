d0c0a300474d66c9feeac61b25aac4e3f06ca10e
dtus <ledoantu04@gmail.com>
2026-06-02 23:49:30 +0700

    khoi tao du an tailwind voi vite

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  return (
+    <>
+      <h1 className="text-red-500 text-4xl font-bold text-center mt-10">
+        Hello world
+      </h1>
+    </>
+  )
+}
+
+export default App
